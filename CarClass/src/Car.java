af7869a835a25bbeff73f06ef615399978805801
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-08 23:57:22 +0530

    Car class

---
+++ CarClass/src/Car.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Car {
+
+    private int year;
+    private String colour;
+    private String make;
+    private String model;
+
+
+    public String displayInfo(String year, String colour, String make, String model) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sadan" +make);
+
+    }
+}

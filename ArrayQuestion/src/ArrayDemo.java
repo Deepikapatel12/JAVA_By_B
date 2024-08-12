8e3d239c762636d4424edb121272c043865a4310
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-12 20:33:43 +0530

    Array

--- ArrayQuestion/src/ArrayDemo.java
+++ ArrayQuestion/src/ArrayDemo.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ArrayDemo {
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] array={10,20,30,40,50};
+        int [] array1={10,20,30,40,80};
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == array1[i]) {
+                System.out.println("Array are same");
+                break;
+            } else {
+                System.out.println("Array are different");
+                break;
+
+            }
+//            System.out.println("array size is not equal");
+        }
+    }
 }
--- ArrayQuestion/src/ThirdArrayCopy.java
+++ ArrayQuestion/src/ThirdArrayCopy.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ThirdArrayCopy {
+public class ThirdArrayCopy {
+    public static void main(String[] args) {
+//        WAP to copy first array at the end of second array by creating third array
+//        both the array can have different size
+
+        int[] arr = {1,2,3,4,5};
+        int [] arr1 = {5,3,2,5,4};
+        int [] arr3 = new int[arr.length + arr1.length];
+          
+    }
 }

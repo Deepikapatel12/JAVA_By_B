381645364044f27c51e8c888c7f71ab76ed179b3
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-12 20:34:21 +0530

    Even number

--- EvenNumber/src/DuplicateChar.java
+++ EvenNumber/src/DuplicateChar.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DuplicateChar {
+import java.util.Scanner;
+
+public class DuplicateChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Number");
+        String number = scanner.nextLine();
+        for (int i = 1; i < number.length(); i++) {
+            char digitChar = number.charAt(i);
+            int digit = Integer.parseInt(digitChar + "");
+            for(int j = i+1; j <= i; j++) {
+
+                if(digitChar(j) == number && digitChar(i) == number)
+                {
+                    System.out.println(digitChar);
+                }
+            }
+        }
+    }
 }
--- EvenNumber/src/EvenNumber.java
+++ EvenNumber/src/EvenNumber.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class EvenNumber {
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 10 digit number");
+        String number = scan.nextLine();
+        for (int i = 1; i < number.length(); i++) {
+            char digitChar = number.charAt(i);
+            int digit = Integer.parseInt(digitChar + "");
+            if (digit % 2 == 0) {
+                System.out.println("Even number" + digit);
+            }
+
+        }
+    }
 }

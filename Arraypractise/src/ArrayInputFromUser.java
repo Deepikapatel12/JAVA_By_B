1f7af2432511a08041d42e64b6a7e3e176803121
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-14 01:07:59 +0530

    Array

--- Arraypractise/src/ArrayInputFromUser.java
+++ Arraypractise/src/ArrayInputFromUser.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ArrayInputFromUser {
+    public static void main(String[] args) {
+        //WAP to accept the elements from user. Calculate and print the sum and average of the elements.
+        int [] array = new int [5];
+        int sum=0;
+        int average=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("scanner = " + scanner);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("i = " + i);
+
+        }
+    }
 }

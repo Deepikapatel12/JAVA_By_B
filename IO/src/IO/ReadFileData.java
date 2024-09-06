4ff9f252af9b7e7f68bae10f5a6a0cfda030ded3
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-06 23:22:14 +0530

    IO

--- IO/src/IO/ReadFileData.java
+++ IO/src/IO/ReadFileData.java
@@ -1,4 +1,26 @@
-package IO;
+
+package com.example.fileread.io;
+
+import java.io.*;
 
 public class ReadFileData {
+    // define a method that reads the file and
+    // return a StringBuilder which contains file data
+    public StringBuilder readData(String fileName)
+            throws FileNotFoundException, IOException {
+        StringBuilder fileData = new StringBuilder();
+        // reading the file data
+        File file = new File(fileName);
+        FileReader fileReader = new FileReader(file);
+        //read line by line
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = "";
+        while (line != null) {
+            line = bufferedReader.readLine();
+            if(line != null) {
+                fileData.append(line);
+            }
+        }
+        return fileData;
+    }
 }

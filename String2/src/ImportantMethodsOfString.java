faa7e7f80f3fdd5c1fbec2b5a7f4a68c661f8696
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-07 21:59:36 +0530

    String

--- String2/src/ImportantMethodsOfString.java
+++ String2/src/ImportantMethodsOfString.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ImportantMethodsOfString {
+    public static void main(String[] args) {
+        String input = "ramesh Sharma";
+        //to get the length of string
+        int length = input.length();
+        //getting character from string by using charAt method char charAt(int index)
+        char firstChar = input.charAt(0);
+        //getting index of character from string int indexO(int charcter)method
+        //will return the first occurred index if the character is present int the string
+        //or else will return -1
+int indexOfCharA = input.indexOf('a');
+
+    }
 }
